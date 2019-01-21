87a2ad46188dc4fb7c3d14c96199357d9430b5f7
Ho Minh Tien <buckblack44@gmail.com>
2019-01-21 16:56:25 +0700

    .

---
+++ back-end/todoapi/todoapi/Controllers/TodoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using todoapi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace todoapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> Get()
+        {
+            return await _context.todoItems.ToListAsync();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>>Get(long id)
+        {
+            var todoItem = await _context.todoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> Post(TodoItem todoItem)
+        {
+            _context.todoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Get", new { id = todoItem.Id }, todoItem);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- database/QuanLyGiaoVu.sql
+++ database/QuanLyGiaoVu.sql
@@ -92,11 +92,38 @@ left join GIANGDAY gd on gd.MAGV=gv.MAGV
 where gd.MAGV is null
 
 --14.***	Tìm giáo viên (mã giáo viên, họ tên) không được phân công giảng dạy bất kỳ môn học nào thuộc khoa giáo viên đó phụ trách.
-select * from GIAOVIEN gv
-where gv.MAGV not in (
-			select gd.MAGV from MONHOC mh, GIANGDAY gd
-			where mh.MAMH=gd.MAMH and mh.MAKHOA = gv.MAKHOA
-			)
+select mh.MAKHOA,gv.MAGV,mh.MAMH 
+from GIAOVIEN gv, GIANGDAY gd,MONHOC mh
+where gd.MAGV=gv.MAGV and mh.MAMH=gd.MAMH
+except
+select distinct k.MAKHOA,gv.MAGV,mh.MAMH 
+from khoa k, GIAOVIEN gv,MONHOC mh
+where k.MAKHOA=gv.MAKHOA and k.MAKHOA=mh.MAKHOA
+
+--****************************************
+select distinct gv.MAGV,gv.HOTEN
+from GIAOVIEN gv, GIANGDAY gd,MONHOC mh
+where gd.MAGV=gv.MAGV and mh.MAMH=gd.MAMH
+and gv.MAGV not in
+		(
+		select distinct gv2.MAGV 
+		from khoa k2, GIAOVIEN gv2,MONHOC mh2
+		where k2.MAKHOA=gv2.MAKHOA and k2.MAKHOA=mh2.MAKHOA
+		and mh.MAKHOA=mh2.MAKHOA and mh.MAMH=mh2.MAMH
+		)
+--***************************************
+select distinct gv.MAGV,gv.HOTEN
+from GIAOVIEN gv, GIANGDAY gd,MONHOC mh
+where gd.MAGV=gv.MAGV and mh.MAMH=gd.MAMH
+and not exists
+		(
+		select distinct gv2.MAGV 
+		from khoa k2, GIAOVIEN gv2,MONHOC mh2
+		where k2.MAKHOA=gv2.MAKHOA and k2.MAKHOA=mh2.MAKHOA
+		and mh.MAKHOA=mh2.MAKHOA and mh.MAMH=mh2.MAMH
+		and gv.MAGV=gv2.MAGV
+		)
+
 
 --15.	Tìm họ tên các học viên thuộc lớp “K11” thi một môn bất kỳ quá 3 lần vẫn “Khong dat” hoặc thi lần thứ 2 môn CTRR được 5 điểm.
 select * from HOCVIEN hv
@@ -110,20 +137,63 @@ where hv.MALOP='K11' and (hv.MAHV in (select mahv from
 								where kq2.MAMH='CTRR' and kq2.LANTHI=2 and kq2.DIEM=5
 								)
 
+--****************************************
+select hv.MAHV,hv.HO,hv.TEN,kq.MAMH
+from HOCVIEN hv
+join KETQUATHI kq on hv.MAHV=kq.MAHV
+where hv.MALOP='K11' and kq.KETQUA='Khong Dat'
+group by hv.MAHV,hv.HO,hv.TEN,kq.MAMH
+having count(kq.MAMH)>=3
+union
+select hv.MAHV,hv.HO,hv.TEN,kq.MAHV
+from HOCVIEN hv
+join KETQUATHI kq on hv.MAHV=kq.MAHV
+where hv.MALOP='K11' and kq.KETQUA='CTRR'
+and kq.LANTHI=2 and kq.DIEM=5
+
+
 --16. Tìm họ tên giáo viên dạy môn CTRR cho ít nhất hai lớp trong cùng một học kỳ của một năm học.
-select gv.HOTEN 
+select gv.MAGV,gv.HOTEN 
 from GIAOVIEN gv,(
 				select gd.MAMH,gd.MAGV,gd.HOCKY,gd.NAMHOC,count(*) as so_lop from GIANGDAY gd
+				where gd.MAMH='CTRR'
 				group by gd.MAMH,gd.MAGV,gd.HOCKY,gd.NAMHOC
 				having COUNT(*)>=2) as tmp
 where gv.MAGV=tmp.MAGV
+--******************************************
+select gv.MAGV,gv.HOTEN,gd.HOCKY,gd.NAMHOC from GIAOVIEN gv
+join GIANGDAY gd on gv.MAGV=gd.MAGV
+where gd.MAMH='CTRR'
+group by gv.MAGV,gv.HOTEN,gd.HOCKY,gd.NAMHOC
+having count(gd.MALOP)>=2
+
 
 --17.	Danh sách học viên và điểm thi môn CSDL (chỉ lấy điểm của lần thi sau cùng).
-select hv.HO,hv.TEN,kq1.DIEM from KETQUATHI kq1,HOCVIEN hv
+select hv.MAHV,hv.HO,hv.TEN,kq1.DIEM,kq1.LANTHI from KETQUATHI kq1,HOCVIEN hv
 where hv.MAHV=kq1.MAHV and kq1.LANTHI=(
-				select MAX(LANTHI) from KETQUATHI kq2
+				select MAX(kq2.LANTHI) from KETQUATHI kq2
 				where kq1.MAHV=kq2.MAHV and kq1.MAMH=kq2.MAMH and kq2.MAMH='CSDL'
 				)
+--******************************************
+select hv.MAHV,hv.HO,hv.TEN,kq1.DIEM,kq1.LANTHI from KETQUATHI kq1,HOCVIEN hv
+where hv.MAHV=kq1.MAHV and kq1.MAMH='CSDL' and kq1.LANTHI>=all(
+				select kq2.LANTHI from KETQUATHI kq2
+				where kq1.MAHV=kq2.MAHV and kq2.MAMH='CSDL'
+				)
+--*******************************************
+select hv.MAHV,hv.HO,hv.TEN,kq1.DIEM,kq1.LANTHI from KETQUATHI kq1,HOCVIEN hv,
+	(
+	select kq2.MAHV,kq2.MAMH,MAX(kq2.LANTHI) as lan_thi from KETQUATHI kq2
+	where kq2.MAMH='CSDL'
+	group by kq2.MAHV,kq2.MAMH
+	) as tmp
+where hv.MAHV=kq1.MAHV and kq1.LANTHI=tmp.lan_thi and tmp.MAHV=kq1.MAHV and tmp.MAMH=kq1.MAMH
+
+--**************************************
+select hv.MAHV,hv.HO,hv.TEN,kq.DIEM,kq.LANTHI
+from HOCVIEN hv
+join KETQUATHI kq on hv.MAHV=kq.MAHV
+where kq.MAMH='CTRR'
 
 --18. Danh sách học viên và điểm thi môn “Co So Du Lieu” (chỉ lấy điểm cao nhất của các lần thi).
 select hv.HO,hv.TEN,kq1.DIEM from KETQUATHI kq1,HOCVIEN hv
@@ -139,30 +209,70 @@ where kh.NGAYTL=(select MIN(kh2.NGAYTL) from KHOA kh2)
 --20.	Có bao nhiêu giáo viên có học hàm là “GS” hoặc “PGS”.
 select COUNT(*) as so_luong from GIAOVIEN
 where HOCHAM='GS' or HOCHAM='PGS'
+--*******************************
+select HOCHAM,COUNT(*) as so_luong from GIAOVIEN
+where HOCHAM='GS' or HOCHAM='PGS'
+group by HOCHAM
+--*******************************
+select sum(case when gv.HOCHAM='GS' then 1 end) GS,sum(case when gv.HOCHAM='PGS' then 1 end) PGS
+from GIAOVIEN gv
 
 --21.	Thống kê có bao nhiêu giáo viên có học vị là “CN”, “KS”, “Ths”, “TS”, “PTS” trong mỗi khoa.
 select kh.MAKHOA,kh.TENKHOA, count(*) as so_luong from GIAOVIEN gv, KHOA kh
 where gv.MAKHOA=kh.MAKHOA and gv.HOCVI in ('CN', 'KS', 'Ths', 'TS', 'PTS')
 group by kh.MAKHOA,kh.TENKHOA
+--******************************
+select kh.MAKHOA,kh.TENKHOA,gv.HOCVI, count(*) as so_luong from GIAOVIEN gv, KHOA kh
+where gv.MAKHOA=kh.MAKHOA and gv.HOCVI in ('CN', 'KS', 'Ths', 'TS', 'PTS')
+group by kh.MAKHOA,kh.TENKHOA,gv.HOCVI
+order by kh.MAKHOA
 
 --22.	Mỗi môn học thống kê số lượng học viên theo kết quả (đạt và không đạt).
 select kq.MAMH,mh.TENMH,kq.KETQUA,COUNT(*) as so_luong from monhoc mh,(select distinct kq2.MAHV,kq2.MAMH,kq2.KETQUA from KETQUATHI kq2) kq
 where mh.MAMH=kq.MAMH
 group by kq.MAMH,mh.TENMH,kq.KETQUA
+order by kq.MAMH
+--********************************************
+select kq.MAMH,mh.TENMH, sum(case when kq.KETQUA='Dat' then 1 end) 'Dat',sum(case when kq.KETQUA='Khong Dat' then 1 end) 'Khong Dat' 
+from monhoc mh,KETQUATHI kq
+where mh.MAMH=kq.MAMH
+group by kq.MAMH,mh.TENMH
+order by kq.MAMH
 
 --23.	Tìm giáo viên (mã giáo viên, họ tên) là giáo viên chủ nhiệm của một lớp, đồng thời dạy cho lớp đó ít nhất một môn học.
-select * from GIAOVIEN gv, LOP l
+select gv.MAGV,gv.HOTEN from GIAOVIEN gv, LOP l
 where l.MAGVCN=gv.MAGV
 and exists (select * from GIANGDAY gd where gd.MAGV=l.MAGVCN and gd.MALOP=l.MALOP)
 
 --24.	Tìm họ tên lớp trưởng của lớp có sỉ số cao nhất.
-select hv.HO,hv.TEN from LOP l,HOCVIEN hv
+select hv.MAHV,hv.HO,hv.TEN  
+from lop l join HOCVIEN hv on l.TRGLOP=hv.MAHV
+where l.SISO>=all(select l1.siso from LOP l1)
+
+--**********************************
+select hv.MAHV,hv.HO,hv.TEN from LOP l,HOCVIEN hv
 where l.TRGLOP=hv.MAHV and l.MALOP in (
 								select hv2.MALOP from HOCVIEN hv2
 								group by hv2.MALOP
 								having count(*)>=all(select count(*) from HOCVIEN hv3
 													group by hv3.MALOP)
 								)
+--***********************************
+with temp
+as(
+	select hv.malop,count(hv.mahv) siso
+	from HOCVIEN hv
+	join KETQUATHI kq on hv.MAHV=kq.MAHV
+	group by hv.MALOP
+)
+select hv.mahv,hv.ho,hv.ten
+from temp
+	join lop on lop.MALOP=temp.MALOP
+	join HOCVIEN hv on hv.MAHV=LOP.TRGLOP
+where temp.siso>=all(select temp1.siso from temp temp1)
+
+select * from HOCVIEN hv, KETQUATHI kq
+where hv.MAHV=kq.MAHV
 
 --25.* Tìm họ tên những LOPTRG thi không đạt quá 3 môn (mỗi môn đều thi không đạt ở tất cả các lần thi).
 select hv.MAHV,hv.HO,hv.TEN from LOP l,HOCVIEN hv
@@ -173,8 +283,26 @@ where l.TRGLOP=hv.MAHV and hv.MAHV=
 																where kq2.KETQUA='Dat')
 								group by kq1.MAHV,kq1.MAMH) as tmp
 								group by tmp.MAHV
-								having count(*)>3
+								having count(*)>=3
 								)
+
+--**********************************
+select hv.MAHV,hv.ho,hv.ten,l.MALOP
+from lop l join HOCVIEN hv on l.TRGLOP=hv.MAHV
+
+
+with tmp
+as
+(select kq.MAHV,kq.MAMH,count(kq.LANTHI) tong_lan_thi,
+	sum(case when kq.KETQUA='Khong Dat' then 1 end) lanthi_khongdat
+ from KETQUATHI kq
+group by kq.MAHV,kq.MAMH
+having count(kq.LANTHI)=sum(case when kq.KETQUA='Khong Dat' then 1 end)
+)
+select tmp.mahv from tmp
+group by tmp.MAHV
+having count(*)>=3
+
 
 --26.	Tìm học viên (mã học viên, họ tên) có số môn đạt điểm 9,10 nhiều nhất.
 select * from HOCVIEN hv
@@ -213,6 +341,12 @@ select tmp1.MAGV,tmp1.HOCKY,tmp1.NAMHOC,tmp1.so_mon,tmp2.so_lop from
 	group by gd2.MAGV,gd2.HOCKY,gd2.NAMHOC) as tmp2
 where tmp1.HOCKY=tmp2.HOCKY and tmp1.MAGV=tmp2.MAGV and tmp1.NAMHOC=tmp2.NAMHOC
 
+--**********************************************
+select gd.HOCKY,gd.NAMHOC,gv.HOTEN,count(distinct gd.MAMH) sl_mh, count(distinct gd.MALOP) sl_lop
+from GIAOVIEN gv join GIANGDAY gd on gv.MAGV=gd.MAGV
+group by gd.HOCKY,gd.NAMHOC,gv.HOTEN
+
+
 --29.**	Trong từng học kỳ của từng năm, tìm giáo viên (mã giáo viên, họ tên) giảng dạy nhiều nhất.????
 select tmp1.MAGV,tmp1.HOCKY,tmp1.NAMHOC from 
 	(select gv.MAGV,gv.NAMHOC,gv.HOCKY,count(gv.MAMH) as so_mon from GIANGDAY gv
@@ -224,6 +358,11 @@ select tmp1.MAGV,tmp1.HOCKY,tmp1.NAMHOC from
 	group by NAMHOC,HOCKY
 	) as tmp2
 where tmp1.HOCKY=tmp2.HOCKY and tmp1.NAMHOC=tmp2.NAMHOC and tmp1.so_mon=tmp2.so_mon
+
+--******************************************
+select gd.HOCKY,gd.NAMHOC,gv.HOTEN,count(distinct gd.MAMH) sl_mh, count(distinct gd.MALOP) sl_lop
+from GIAOVIEN gv join GIANGDAY gd on gv.MAGV=gd.MAGV
+group by gd.HOCKY,gd.NAMHOC,gv.HOTEN
 
 --30.	Tìm môn học (mã môn học, tên môn học) có nhiều học viên thi không đạt (ở lần thi thứ 1) nhất.
 select MAMH,TENMH from MONHOC where MAMH in
