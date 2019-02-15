bf6a99d303dc6418a94a99ff6bf61e21544ca830
Ho Minh Tien <buckblack44@gmail.com>
2019-02-15 12:06:14 +0700

    .

---
+++ back-end/codefirst/codefirst/Models/Db.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codefirst.Models
+{
+    public class Db:DbContext
+    {
+        public Db(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Major> Majors { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ back-end/codefirst/codefirst/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace codefirst.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+        public string Code { get; set; }
+        [Column("StudentName", TypeName = "nText")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public int MajorID { get; set; }
+        [ForeignKey("MajorID")]
+        public virtual Major Major { get; set; }
+    }
+}
